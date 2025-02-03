6cff22d627b0692b6b9b3e60ba47063fec98cd42
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-02-03 18:21:05 +0100

    finish the ui

--- src/main/java/com/fsdm/cv/home.java
+++ src/main/java/com/fsdm/cv/home.java
@@ -2,40 +2,61 @@ package com.fsdm.cv;
 
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.*;
 
 public class home {
-    private JButton button1;
+    private JButton ouvrirButton;
     private JPanel screen;
+    private JButton enregistrerButton;
+    private JButton quitterButton;
+    private JButton laplacienButton;
+    private JButton sobelButton;
+    private JButton gradientButton;
+    private JButton prewittButton;
+    private JButton robertButton;
+    private JButton moyenneur33Button;
+    private JButton moyenneur55Button;
+    private JButton coniqueButton;
+    private JButton gaussienne33Button;
+    private JButton gaussienne55Button;
+    private JButton pyramidalButton;
+    private JButton medianButton;
+    private JButton inversionButton;
+    private JButton binarisationButton;
+    private JTextField suileTextField;
+    private JButton contrasteButton;
+    private JTextField contrasteTextField;
+    private JButton divisionButton;
+    private JButton niveauDeGrisButton;
+    private JButton histogramButton;
+    private JButton contrast_amilorationButton;
+    private JTextField pContrast;
+    private JTextField bContrast;
+    private JButton FPBButton;
+    private JButton FPBBButton;
+    private JButton FPHButton;
+    private JButton FPHBButton;
+    private JPanel originalmage;
+    private JPanel filtredImage;
 
     public home() {
-        button1.addActionListener(new ActionListener() {
-            /**
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                button1.setText("its clicked");
-            }
-        });
-        button1.addComponentListener(new ComponentAdapter() {
-            /**
-             * @param e the event to be processed
-             */
-            @Override
-            public void componentResized(ComponentEvent e) {
 
-                super.componentResized(e);
-            }
-        });
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("home");
-        frame.setContentPane(new home().screen);
+
+
+        JPanel panel = new JPanel();
+        panel.add(new home().screen);
+        panel.setPreferredSize(new Dimension(1000, 680));
+        panel.setMinimumSize(new Dimension(300, 220));
+        panel.setMaximumSize(new Dimension(1500, 1200));
+
+
+        frame.setContentPane(panel);
+
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
